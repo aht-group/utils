11a5e82b92271b1a2d35aecfebcbd2bed5fa5f84
Thorsten-K <t.kisner@web.de>
2020-03-07 17:40:25 +0100

    IoDbReplicationHost removed

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/io/db/JeeslDbDumpHost.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/io/db/JeeslDbDumpHost.java
@@ -4,18 +4,14 @@ import java.io.Serializable;
 
 import org.jeesl.interfaces.model.marker.jpa.EjbPersistable;
 import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
-import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
 import org.jeesl.interfaces.model.system.locale.JeeslDescription;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
 import org.jeesl.interfaces.model.system.locale.status.JeeslStatus;
-import org.jeesl.interfaces.model.system.option.JeeslOptionRestDescription;
 
 public interface JeeslDbDumpHost <L extends JeeslLang, D extends JeeslDescription,
 									S extends JeeslStatus<S,L,D>,
 									G extends JeeslGraphic<L,D,?,?,?>>
 							extends Serializable,EjbPersistable,
-									JeeslOptionRestDescription,
-									EjbWithCodeGraphic<G>,//UtilsStatusFixedCode,
 									JeeslStatus<S,L,D>
 {	
 
