d3e4feeec3a2618c5c346b0cc205be479b9311c3
root <root@ubuntu>
2020-03-10 08:33:29 -0700

    JeeslCurlDocker improve

--- client/src/main/java/org/jeesl/client/app/JeeslCurlDocker.java
+++ client/src/main/java/org/jeesl/client/app/JeeslCurlDocker.java
@@ -3,9 +3,15 @@ package org.jeesl.client.app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
+import org.jeesl.model.json.system.io.ssi.docker.JsonDockerContainer;
+import org.jeesl.model.json.system.io.ssi.docker.JsonDockerContainerPorts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.sf.exlp.util.io.LoggerInit;
 
@@ -50,6 +56,26 @@ public class JeeslCurlDocker
 		LoggerInit loggerInit = new LoggerInit("log4j.xml");
 		loggerInit.addAltPath("jeesl/client/config");
 		loggerInit.init();
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		try 
+		{
+			List<JsonDockerContainer> jDc = objectMapper.readValue(jCD.runProcess(jCD.xGet("http://v1.40/containers/json?all")), new TypeReference<List<JsonDockerContainer>>(){});
+			logger.info("Id:  " +jDc.get(0).getId());
+			logger.info("Image:  " +jDc.get(0).getImage());
+			logger.info("State:  " +jDc.get(0).getState());
+			logger.info("Status:  " +jDc.get(0).getStatus());
+			
+			List<JsonDockerContainerPorts> ports = jDc.get(0).getPorts();
+			for (JsonDockerContainerPorts p : ports)
+			{
+				logger.info("IP: "+p.getIp());
+				logger.info("PrivatePort: "+p.getPrivatePort());
+				logger.info("PublicPort: "+p.getPublicPort());
+				logger.info("Type: "+p.getType());
+			}
+			
+		} catch (IOException | IndexOutOfBoundsException | InterruptedException e1) {logger.info("There are no running containers!");}
 		
 		try {logger.info(jCD.runProcess(jCD.xGet("http://v1.40/containers/json?all")));} catch (IOException | InterruptedException e) {}
 	
