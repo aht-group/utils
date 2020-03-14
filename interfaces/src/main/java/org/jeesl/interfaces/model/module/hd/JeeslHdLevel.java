1a21a41011107441efc23e0b6b1d69ecf17dad1f
Thorsten-K <t.kisner@web.de>
2020-03-14 10:34:44 +0100

    Helpdesk classes added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/hd/JeeslHdLevel.java
@@ -0,0 +1,23 @@
+package org.jeesl.interfaces.model.module.hd;
+
+import java.io.Serializable;
+
+import org.jeesl.interfaces.model.marker.jpa.EjbPersistable;
+import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
+import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
+import org.jeesl.interfaces.model.system.locale.JeeslDescription;
+import org.jeesl.interfaces.model.system.locale.JeeslLang;
+import org.jeesl.interfaces.model.system.locale.status.JeeslStatus;
+import org.jeesl.interfaces.model.with.primitive.code.EjbWithCode;
+
+public interface JeeslHdLevel <L extends JeeslLang, D extends JeeslDescription,
+									S extends JeeslStatus<S,L,D>,
+									G extends JeeslGraphic<L,D,?,?,?>>
+					extends Serializable,EjbPersistable,
+								EjbWithCode,
+								//JeeslStatusFixedCode,JeeslOptionRestDownload,
+								EjbWithCodeGraphic<G>,
+								JeeslStatus<S,L,D>
+{	
+
+}
