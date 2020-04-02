227e974149da1b97fbd4b8c69e76ebdc33e5a14f
Thorsten-K <t.kisner@web.de>
2020-04-02 09:30:42 +0200

    cms added to sec.document

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/security/doc/JeeslSecurityDocument.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/security/doc/JeeslSecurityDocument.java
@@ -6,15 +6,21 @@ import org.jeesl.interfaces.model.io.cms.JeeslIoCms;
 import org.jeesl.interfaces.model.io.cms.JeeslIoCmsSection;
 import org.jeesl.interfaces.model.marker.jpa.EjbSaveable;
 import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityView;
+import org.jeesl.interfaces.model.with.parent.EjbWithParentAttributeResolver;
 import org.jeesl.interfaces.model.with.primitive.position.EjbWithPositionVisible;
 
 public interface JeeslSecurityDocument<V extends JeeslSecurityView<?,?,?,?,?,?>,
 										DOC extends JeeslIoCms<?,?,?,SEC,?>,
 										SEC extends JeeslIoCmsSection<?,SEC>>
-			extends Serializable,EjbSaveable,EjbWithPositionVisible
-{	
+			extends Serializable,EjbSaveable,EjbWithPositionVisible,EjbWithParentAttributeResolver
+{
+	public enum Attributes{view}
+	
 	V getView();
 	void setView(V view);
+	
+	DOC getDocument();
+	void setDocument(DOC document);
 	
 	SEC getSection();
 	void setSection(SEC section);
