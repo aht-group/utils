036402d80eec40e66ae96faabe2de3b7378b5dbc
Thorsten-K <t.kisner@web.de>
2020-04-02 08:50:20 +0200

    Cms added to SecurityDocument

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/security/doc/JeeslSecurityDocument.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/security/doc/JeeslSecurityDocument.java
@@ -2,18 +2,20 @@ package org.jeesl.interfaces.model.system.security.doc;
 
 import java.io.Serializable;
 
+import org.jeesl.interfaces.model.io.cms.JeeslIoCms;
 import org.jeesl.interfaces.model.io.cms.JeeslIoCmsSection;
 import org.jeesl.interfaces.model.marker.jpa.EjbSaveable;
 import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityView;
 import org.jeesl.interfaces.model.with.primitive.position.EjbWithPositionVisible;
 
 public interface JeeslSecurityDocument<V extends JeeslSecurityView<?,?,?,?,?,?>,
-											S extends JeeslIoCmsSection<?,S>>
+										DOC extends JeeslIoCms<?,?,?,SEC,?>,
+										SEC extends JeeslIoCmsSection<?,SEC>>
 			extends Serializable,EjbSaveable,EjbWithPositionVisible
 {	
 	V getView();
 	void setView(V view);
 	
-	S getSection();
-	void setSection(S section);
+	SEC getSection();
+	void setSection(SEC section);
 }
