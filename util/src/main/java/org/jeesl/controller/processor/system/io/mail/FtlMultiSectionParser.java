8c11d4a7438daed7bc3ee87e15db1e88596eda6e
Thorsten-K <t.kisner@web.de>
2020-05-25 12:58:47 +0200

    dummy parser added

---
+++ util/src/main/java/org/jeesl/controller/processor/system/io/mail/FtlMultiSectionParser.java
@@ -0,0 +1,18 @@
+package org.jeesl.controller.processor.system.io.mail;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FtlMultiSectionParser
+{
+	final static Logger logger = LoggerFactory.getLogger(FtlMultiSectionParser.class);
+		
+	public Map<String,List<String>> parse(String text)
+	{
+		return new HashMap<>();
+	}
+}
