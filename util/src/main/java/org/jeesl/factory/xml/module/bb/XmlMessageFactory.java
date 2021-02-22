781bf3cdea7eda0e16b59579d4d851ffa9b477e3
Lasse Petersen <petersen@aht-group.com>
2021-02-22 13:36:04 +0100

    XmlMessageFactory added build(Date, Subject)

--- util/src/main/java/org/jeesl/factory/xml/module/bb/XmlMessageFactory.java
+++ util/src/main/java/org/jeesl/factory/xml/module/bb/XmlMessageFactory.java
@@ -1,5 +1,7 @@
 package org.jeesl.factory.xml.module.bb;
 
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import org.jeesl.model.xml.module.bb.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,4 +12,5 @@ public class XmlMessageFactory
 	
 	public static Message build() {return new Message();}
 	public static Message build(String subject) {Message xml = build(); xml.setSubject(subject); return xml;}
+	public static Message build(XMLGregorianCalendar date, String subject) {Message xml = build(); xml.setSubject(subject); xml.setDate(date); return xml;}
 }
