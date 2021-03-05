511fa9e3f1c6d58e9bcfd69d30074fc8884a58e8
Kevin Meier <meier@aht-group.com>
2021-03-05 13:24:31 +0100

    Statusbar Dialog button fix, responsive image gallery, tree

--- jsf/src/main/resources/META-INF/resources/jeeslJs/jeesl.responsive.js
+++ jsf/src/main/resources/META-INF/resources/jeeslJs/jeesl.responsive.js
@@ -5,14 +5,49 @@
  * 
  * We also need to set or remove a CSS class to show or hide the menu when clicking the burger menu icon.
  */
-let style;
+let menuHeightStyle;
+let treeHeightStyle;
 
 function calculateMenuHeight() {
 	let cssRules = '@media (max-width: 768px) {';
 	
 	$('.jeesl-dropdown-list').each((index, element) => cssRules += '#' + $(element).attr('id') + '.jeesl-active { height: ' + ($(element).children('.jeesl-dropdown-item').length * 50 + 30) + 'px; }');
 	
-	style.append(cssRules + '}');
+	menuHeightStyle.append(cssRules + '}');
+}
+
+//function calculateTreeHeight() {
+//	$('.ui-tree-container > .ui-treenode').each((index, element) => setTreeHeight($(element)));
+//}
+
+function toggleTreeItem() {
+//	let node = $(this).parent().parent();
+//	node.toggleClass('jeesl-active');
+//	
+//	setTreeHeight(findTreeRoot(node));
+	
+	$(this).parent().next('.ui-treenode-children').toggleClass('jeesl-active');
+}
+
+function findTreeRoot(treeNode) {
+	return treeNode.parent().hasClass('ui-treenode-children') ? findTreeRoot(treeNode.parent().parent()) : treeNode;
+}
+
+function setTreeHeight(node) {
+	let height = 0;
+	let childList = node.children('.ui-treenode-children');
+	
+	if (node.hasClass('jeesl-active')) {
+		height += childList.first()
+						   .children('.ui-treenode')
+						   .map((index, item) => setTreeHeight($(item)))
+						   .toArray()
+						   .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+				+ childList.first().children('.ui-tree-droppoint:last-child').length * 4;
+	}
+	
+	childList.height(height);
+	return height + 50;
 }
 
 function toggleMenu() {
@@ -30,11 +65,14 @@ function reloadStatusBar() {
 }
 
 $(function() {
-	style = $('<style>').prop('type', 'text/css').appendTo('head');
+	menuHeightStyle = $('<style>').prop('type', 'text/css').appendTo('head');
+	treeHeightStyle = $('<style>').prop('type', 'text/css').appendTo('head');
 	
 	let menuButtons = $('.jeesl-menu-bar-button');
 	let dropdowns = $('.jeesl-dropdown-list').attr('id', (index, oldValue) => 'jeesl-dropdown-' + index);
 	
 	calculateMenuHeight(dropdowns);
 	menuButtons.click(toggleMenu);
+	
+	$('.ui-tree-toggler').click(toggleTreeItem);
 });
