8929eced57db39ad0bb795213f2b215f6b986a01
Thorsten-K <t.kisner@web.de>
2021-03-17 10:05:16 +0100

    Rating updated

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/rating/JeeslRating.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/rating/JeeslRating.java
@@ -5,12 +5,11 @@ import java.io.Serializable;
 import org.jeesl.interfaces.model.marker.jpa.EjbSaveable;
 import org.jeesl.interfaces.model.system.locale.JeeslDescription;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
-import org.jeesl.interfaces.model.system.locale.status.JeeslStatus;
 import org.jeesl.interfaces.model.with.parent.EjbWithParentAttributeResolver;
 import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
 
 public interface JeeslRating<L extends JeeslLang, D extends JeeslDescription,
-								CATEGORY extends JeeslStatus<CATEGORY,L,D>,
+								CATEGORY extends EjbWithId,
 								DOMAIN extends EjbWithId
 >
 			extends Serializable,EjbWithId,EjbSaveable,EjbWithParentAttributeResolver
