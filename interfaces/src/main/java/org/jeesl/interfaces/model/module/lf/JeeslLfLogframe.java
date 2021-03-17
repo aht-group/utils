127f1e2e8c413eb8eaeca7296637acdd108517f8
manandhar <manandhar@nbsm19.aht-group.com>
2021-03-17 23:23:41 +0100

    Logframe Target time improved

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/lf/JeeslLfLogframe.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/lf/JeeslLfLogframe.java
@@ -2,16 +2,23 @@ package org.jeesl.interfaces.model.module.lf;
 
 import java.io.Serializable;
 
+import org.jeesl.interfaces.model.marker.jpa.EjbRemoveable;
 import org.jeesl.interfaces.model.marker.jpa.EjbSaveable;
+import org.jeesl.interfaces.model.system.locale.JeeslDescription;
+import org.jeesl.interfaces.model.system.locale.JeeslLang;
 import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
 import org.jeesl.interfaces.model.with.primitive.text.EjbWithName;
+import org.jeesl.interfaces.model.with.system.status.JeeslWithLevel;
+import org.jeesl.interfaces.model.with.system.status.JeeslWithType;
 
-public interface JeeslLfLogframe //<M extends JeeslMarkup<?>>
-						extends Serializable,EjbSaveable,
-								EjbWithId,EjbWithName
+public interface JeeslLfLogframe<L extends JeeslLang, D extends JeeslDescription,
+								LFL extends JeeslLfLevel<L,D,LFL,?>,
+								LFT extends JeeslLfType<L,D,LFT,?>>
+						extends Serializable,EjbSaveable,EjbRemoveable,
+								EjbWithId,EjbWithName,
+								JeeslWithType<LFT>,JeeslWithLevel<LFL>
 //,								JeeslWithFileRepositoryContainer<FRC>
-{	
-	public enum Attributes{lastEvent}
-	
+{
+	//public enum Attributes{}
 
 }
