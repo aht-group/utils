cb511f00cbb7e3e185f12d84c78b164c6d03d60f
Thorsten-K <t.kisner@web.de>
2021-04-02 16:04:32 +0200

    Eap7.3 config remaining

--- util/src/main/java/org/jeesl/controller/config/jboss/JbossModuleConfigurator.java
+++ util/src/main/java/org/jeesl/controller/config/jboss/JbossModuleConfigurator.java
@@ -70,7 +70,7 @@ public class JbossModuleConfigurator
 			FileUtils.copyFileToDirectory(MavenArtifactResolver.resolve("net.postgis:postgis-jdbc:2.5.0"),moduleMain);
 
 		}
-		else if(version.equals("7.3.4"))
+		else if(version.equals("7.3"))
 		{
 			FileUtils.copyFileToDirectory(MavenArtifactResolver.resolve("org.postgresql:postgresql:42.2.18"),moduleMain);
 			FileUtils.copyFileToDirectory(MavenArtifactResolver.resolve("net.postgis:postgis-jdbc:2.5.0"),moduleMain);
@@ -213,7 +213,7 @@ public class JbossModuleConfigurator
 			//Find the version in geolatte-geom
 			FileUtils.copyFileToDirectory(MavenArtifactResolver.resolve("com.vividsolutions:jts-core:1.14.0"),moduleMain);
 		}
-		else if(version.equals("7.3.4"))
+		else if(version.equals("7.3"))
 		{
 			//Should match the hibernate version of EAP7.2.x
 			FileUtils.copyFileToDirectory(MavenArtifactResolver.resolve("org.hibernate:hibernate-spatial:5.3.18.Final"),moduleMain);
