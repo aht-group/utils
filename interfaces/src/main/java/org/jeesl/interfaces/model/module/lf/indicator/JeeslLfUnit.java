7a97d0968f51dd73ee743f97a3ed1c8b0e814f18
Thorsten-K <t.kisner@web.de>
2021-06-08 08:40:32 +0200

    RestDownload added for LfUnit

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/lf/indicator/JeeslLfUnit.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/lf/indicator/JeeslLfUnit.java
@@ -4,13 +4,14 @@ import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
 import org.jeesl.interfaces.model.system.locale.JeeslDescription;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
 import org.jeesl.interfaces.model.system.locale.status.JeeslMcsStatus;
+import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
 import org.jeesl.interfaces.model.system.tenant.JeeslTenantRealm;
 
 public interface JeeslLfUnit <L extends JeeslLang, D extends JeeslDescription,
 										R extends JeeslTenantRealm<L,D,R,G>,
 										S extends JeeslMcsStatus<L,D,R,S,G>,
 										G extends JeeslGraphic<L,D,?,?,?>>
-			extends JeeslMcsStatus<L,D,R,S,G>
+			extends JeeslMcsStatus<L,D,R,S,G>, JeeslOptionRestDownload
 					
 {
 	public enum Attributes{realm,rref}
